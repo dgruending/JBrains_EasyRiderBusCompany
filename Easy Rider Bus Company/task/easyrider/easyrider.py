1780345d1c8226653e0993d8c619277690c5e5d3
dgruending <dgruending23@gmail.com>
2021-10-19 10:52:14 +0000

    Completed stage 1

--- Easy Rider Bus Company/task/easyrider/easyrider.py
+++ Easy Rider Bus Company/task/easyrider/easyrider.py
@@ -18,12 +18,27 @@ def validate_data(data):
         # check if stop_id is int and provided (not provided would be ""[string])
         if not isinstance(stop["stop_id"], int):
             stop_id_errors += 1
-        # check stop_name TODO
+        # check stop_name
+        if not isinstance(stop["stop_name"], str) or stop["stop_name"] == "":
+            stop_name_errors += 1
         # check if next_stop is int and provided (not provided would be ""[string])
         if not isinstance(stop["next_stop"], int):
             next_stop_errors += 1
-        # check stop_type TODO
-        # check a_time TODO
+        # check stop_type
+        if not isinstance(stop["stop_type"], str) or len(stop["stop_type"]) > 1:
+            stop_type_errors += 1
+        # check a_time
+        if not isinstance(stop["a_time"], str) or stop["a_time"] == "":
+            a_time_errors += 1
+
+    print("Type and required field validation: {} errors".format(bus_id_errors + stop_id_errors + stop_name_errors
+                                                                 + next_stop_errors + stop_type_errors + a_time_errors))
+    print(f"bus_id: {bus_id_errors}")
+    print(f"stop_id: {stop_id_errors}")
+    print(f"stop_name: {stop_name_errors}")
+    print(f"next_stop: {next_stop_errors}")
+    print(f"stop_type: {stop_type_errors}")
+    print(f"a_time: {a_time_errors}")
 
 
 if __name__ == '__main__':
